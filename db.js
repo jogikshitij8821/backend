bec987e175a9ff7e093a75c974474a77ba7f841f
kshitij jogi <kshitijjogi856@gmail.com>
2023-09-22 10:00:30 +0530

    updated

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect(`mongodb://127.0.0.1:27017/blogApp`, {
+      const conn = await mongoose.connect(`https://backend-x9r5.onrender.com/blogApp`, {
         useNewUrlParser: true,
       });
       console.log(`MongoDB Connected: ${conn.connection.host}`);
